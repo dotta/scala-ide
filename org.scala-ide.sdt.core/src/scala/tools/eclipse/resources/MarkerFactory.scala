b70688d8407dc73bfbf548d13362046f0bcba2d8
Mirco Dotta <mirco.dotta@typesafe.com>
2012-06-29 16:57:05 +0200

    Truncate too long build error messages displayed in resources' marker
    Markers are a general mechanism for associating notes and meta-data with
    resources.  The Eclipse environments dictates that the text message passed to a
    marker should not be bigger than 21000 chars, or an assertion failure saying
    
    > Marker property value is too long
    
    is thrown on the user's face.
    
    The actual logic for resizing the build error problem passed to a marker
    already existed in two places. While the message passed to the created marker
    was correctly truncated in ``FileUtils.buildError``, it was not in
    ``ScalaProject.buildErrors``.
    
    As part of this commit, I've centralized the creation of resource markers in
    ``MarkerFactory``, and build problem markers are now created using
    ``BuildProblemMarker`` (it's a subclass of ``MarkerFactory```).
    
    More cleaning up could be done so that all markers are created only through
    sublcasses of `MarkerFactory``. Though, such a big refactoring seem too risky
    at the moment, as this commit will be merged in ``release/2.0.x`` branch.
    
    Fix #1001107

---
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/resources/MarkerFactory.scala
@@ -0,0 +1,90 @@
+package scala.tools.eclipse.resources
+
+import scala.tools.eclipse.util.EclipseUtils
+
+import org.eclipse.core.resources.IMarker
+import org.eclipse.core.resources.IResource
+import org.eclipse.core.runtime.IProgressMonitor
+
+object MarkerFactory {
+  case class Position(offset: Int, length: Int, line: Int)
+}
+
+/**
+ * Generic factory for creating resource's markers.
+ *
+ * Markers are a general mechanism for associating notes and meta-data with resources.
+ *
+ * Clients extending {{{MarkerFactory}}} are expected to provide an implementation for {{{markerId}}},
+ * which is a unique identifier for all markers created through a {{{MarkerFactory}}} instance.
+ *
+ * Example:
+ * {{{
+ *     class SettingMarker extends MarkerFactory {
+ *       override def markerId: String = "org.scala-ide.sdt.core.settingProblem"
+ *     }
+ * }}}
+ */
+trait MarkerFactory {
+  /**
+   * Create marker without a source position in the Problem view.
+   * @param resource: The resource to use to create the marker (hence, the marker will be associated to the passed resource)
+   * @param severity: Indicates the marker's error state. Its value can be one of:
+   *                  [IMarker.SEVERITY_ERROR, IMarker.SEVERITY_WARNING, IMarker.SEVERITY_INFO]
+   * @param msg: The text message displayed by the marker. Note, the passed message is truncated to 21000 chars.
+   */
+  def create(resource: IResource, severity: Int, msg: String): Unit = runInWorkspace(resource) { _ =>
+    createMarkerWithAttributes(severity, msg)(resource)
+  }
+
+  /**
+   * Create marker with a source position in the Problem view.
+   * @param resource: The resource to use to create the marker (hence, the marker will be associated to the passed resource)
+   * @param severity: Indicates the marker's error state. Its value can be one of:
+   *                  [IMarker.SEVERITY_ERROR, IMarker.SEVERITY_WARNING, IMarker.SEVERITY_INFO]
+   * @param msg: The text message displayed by the marker. Note, the passed message is truncated to 21000 chars.
+   * @param pos: The source position for the marker.
+   */
+  def create(resource: IResource, severity: Int, msg: String, pos: MarkerFactory.Position): Unit = runInWorkspace(resource) { _ =>
+    (createMarkerWithAttributes(severity, msg) andThen withPos(pos))(resource)
+  }
+
+  private def createMarkerWithAttributes(severity: Int, msg: String): IResource => IMarker =
+    resource => (createMarker andThen update(severity, msg))(resource)
+
+  private def createMarker: IResource => IMarker =
+    _.createMarker(markerId)
+
+  /** A unique identifier for the created marker. */
+  protected def markerId: String
+
+  private def runInWorkspace(resource: IResource)(f: IProgressMonitor => Unit): Unit =
+    EclipseUtils.workspaceRunnableIn(resource.getWorkspace, null)(f)
+
+  private def update(severity: Int, msg: String): IMarker => IMarker = marker => {
+    marker.setAttribute(IMarker.SEVERITY, severity)
+    // Marker attribute values are limited to <= 65535 bytes and setAttribute will assert if they
+    // exceed this. To guard against this we trim to <= 21000 characters ... see
+    // org.eclipse.core.internal.resources.MarkerInfo.checkValidAttribute for justification
+    // of this arbitrary looking number
+    val maxMarkerLen = 21000
+    val trimmedMsg = msg.take(maxMarkerLen)
+
+    val attrValue = trimmedMsg.map {
+      case '\n' | '\r' => ' '
+      case c           => c
+    }
+
+    marker.setAttribute(IMarker.MESSAGE, attrValue)
+    marker
+  }
+
+  private def withPos(position: MarkerFactory.Position): IMarker => IMarker = marker => {
+    if (position.offset != -1) {
+      marker.setAttribute(IMarker.CHAR_START, position.offset)
+      marker.setAttribute(IMarker.CHAR_END, position.offset + math.max(position.length, 1))
+      marker.setAttribute(IMarker.LINE_NUMBER, position.line)
+    }
+    marker
+  }
+}
